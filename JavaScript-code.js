d5d8bedc855151875270cf33cd2804009949e4d2
Amol Vaze <mailmeamol2112@gmail.com>
2020-10-21 22:36:12 -0500

    Update JavaScript-code.js

--- JavaScript-code.js
+++ JavaScript-code.js
@@ -2242,3 +2242,69 @@ async function fetchUsers() {
 }
 
 fetchUsers();
+// ------------------------------------ Median Of Two Sorted Arrays -------------------------------------
+//Input: nums1 = [1,3], nums2 = [2]
+//Output: 2.00000
+
+//Questions:
+// what is a median ?
+// best time complexity ?
+  
+
+//test cases  
+  // nums1 = [1,3,34,90], nums2 = []
+  // nums1 = []. nums2 = []
+
+function bruteForce(nums1 = [], nums2 = []) {
+  const mergedNums = [...nums1, ...nums2].sort((a, b) => a - b);
+  const midPoint = Math.floor(mergedNums.length / 2);
+
+  return mergedNums.length % 2 !== 0
+    ? mergedNums[midPoint]
+    : (mergedNums[midPoint - 1] + mergedNums[midPoint]) / 2;
+}
+
+
+// merge sort
+// complexity O(n + m)
+function findMedianSortedArrays(nums1 = [], nums2 = []) {
+  let i1 = 0;
+  let i2 = 0;
+  const len1 = nums1.length;
+  const len2 = nums2.length;
+  const len = len1 + len2;
+
+  if (len === 0) {
+    return null;
+  }
+
+  const merged = [];
+
+  while (i1 < len1 && i2 < len2) {
+    if (nums1[i1] <= nums2[i2]) {
+      merged.push(nums1[i1++]);
+    } else {
+      merged.push(nums2[i2++]);
+    }
+  }
+
+  while (i1 < len1) {
+    merged.push(nums1[i1++]);
+  }
+  while (i2 < len2) {
+    merged.push(nums2[i2++]);
+  }
+
+  const isOdd = len % 2;
+  if (isOdd) {
+    return merged[(len - 1) / 2];
+  } else {
+    return (merged[merged.length / 2] + merged[merged.length / 2 - 1]) / 2;
+  }
+}
+
+const nums1 = [1,3];
+const nums2 = [2];
+
+console.log(findMedianSortedArrays(nums1, nums2))
+
