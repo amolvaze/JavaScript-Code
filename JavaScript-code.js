13a0be6450d6324a43247a4e145a47285bcb4828
Amol Vaze <mailmeamol2112@gmail.com>
2020-11-23 18:23:14 -0600

    Update JavaScript-code.js

--- JavaScript-code.js
+++ JavaScript-code.js
@@ -2467,3 +2467,20 @@ const nums1 = [1,3];
 const nums2 = [2];
 
 console.log(findMedianSortedArrays(nums1, nums2))
+
+// Two Sum - O(n) solution
+
+function twoSum(nums, target){
+  const comp = new Map()
+  const len = nums.length;
+  for(let i=0; i< len; i++){
+   if(comp[nums[i]] >=0){
+     return [comp[nums[i]], i]
+   }
+   comp[target- nums[i]] = i;
+
+  }
+}
+
+const nums = [2,7,11,15]
+console.log(twoSum(nums, 9))
