679a9850741e9976f8107aa3871058c831f02904
Amol <mailmeamol2112@gmail.com>
2021-04-24 17:40:16 -0400

    cleaning comments

--- FetchAPI-React.js
+++ FetchAPI-React.js
@@ -1,56 +1,46 @@
-import React, {useState} from 'react';
-//import classnames from 'classnames';
-// you should import `lodash` as a whole module
-import lodash from 'lodash';
-import axios from 'axios';
+import React, { useState } from "react";
+import lodash from "lodash";
+import axios from "axios";
 
-const ITEMS_API_URL = 'https://jsonplaceholder.typicode.com/todos';
+const ITEMS_API_URL = "https://jsonplaceholder.typicode.com/todos";
 const DEBOUNCE_DELAY = 500;
 
-// the exported component can be either a function or a class
+export default function Autocomplete({ onSelectItem }) {
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState("");
 
-export default function Autocomplete({onSelectItem}) {
- const[data, setData] = useState([])
- const[value, setValue] = useState('')
-
- const fetchData = () => {
-    //  console.log(value)
-     setData(value.split(''))
+  const fetchData = () => {
+    setData(value.split(""));
     //  axios.get(ITEMS_API_URL, { params: { q: value} })
     //  .then(res => setData(res.data))
- }
+  };
 
- const debounceFetchData = 
-      lodash.debounce(fetchData, DEBOUNCE_DELAY)
+  const debounceFetchData = lodash.debounce(fetchData, DEBOUNCE_DELAY);
 
- function handleChange() { 
-   
+  function handleChange() {
     debounceFetchData();
   }
 
   return (
     <div className="wrapper">
       <div className="control">
-        <input type="text" value = {value} className="input" 
-       onChange={(e) =>{ 
-           setValue(e.target.value);
-           handleChange();
-       }}
-        
-       />
+        <input
+          type="text"
+          value={value}
+          className="input"
+          onChange={(e) => {
+            setValue(e.target.value);
+            handleChange();
+          }}
+        />
       </div>
-      <div className="list is-hoverable" > 
-         { 
-             data.map(d => (
-      <div 
-      key= {d} 
-      onClick = {() => onSelectItem(d)} 
-      className = "list-item">{d}</div>
-  
-             ))
-         }
+      <div className="list is-hoverable">
+        {data.map((d) => (
+          <div key={d} onClick={() => onSelectItem(d)} className="list-item">
+            {d}
+          </div>
+        ))}
       </div>
     </div>
   );
 }
-
